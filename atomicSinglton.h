4235c8412bc5f5c64e905765f929b80e5f73ed06
Alisher Aliev <allyan@walla.com>
2018-07-08 22:09:03 +0300

    Add files via upload
    Implementation of multithread safe template singleton class

---
+++ atomicSinglton.h
@@ -0,0 +1,52 @@
+#ifndef __ATOMIC_SINGLTON__
+#define __ATOMIC_SINGLTON__
+
+
+namespace advcpp
+{
+
+template <typename T> 
+class Singleton
+{
+    public:
+        static T& getInstance();
+
+    private:
+        static T* instance;
+        static volatile int lock;
+        Singleton();
+        ~Singleton();
+
+}; // class Singleton
+
+template <typename T> 
+T* Singleton<T>::instance = 0;
+
+template <typename T> 
+volatile int Singleton<T>::lock = 0;
+
+template <typename T> 
+T& Singleton<T>::getInstance()
+{
+	while (__sync_lock_test_and_set(&lock, 1)) while (lock);
+    // critical section
+    __sync_bool_compare_and_swap(&instance, 0, new T);
+    __sync_lock_release(&lock);
+
+    return *instance;
+}
+
+template <typename T> 
+Singleton<T>::Singleton(){}
+
+
+template <typename T> 
+Singleton<T>::~Singleton()
+{
+	delete instance;
+}
+
+} // namespace advcpp
+
+
+#endif /* __ATOMIC_SINGLTON__ */
